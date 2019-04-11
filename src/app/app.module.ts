07cda4ce24ff47e8b8dca905bf8874b944d8c69d
Marcus Stockton <m.stockton@fargosystems.com>
2019-04-11 14:41:54 +0100

    fleshing out the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { ErrorInterceptorComponent } from './helpers/error-interceptor/error-interceptor.component';
+import { HomeComponent } from './home/home.component';
+import { ItemsComponent } from './items/items.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { OrdersComponent } from './orders/orders.component';
+import { AccountComponent } from './account/account.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    ErrorInterceptorComponent,
+    HomeComponent,
+    ItemsComponent,
+    ReviewsComponent,
+    OrdersComponent,
+    AccountComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/helpers/error-interceptor/error-interceptor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-error-interceptor',
+  templateUrl: './error-interceptor.component.html',
+  styleUrls: ['./error-interceptor.component.css']
+})
+export class ErrorInterceptorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
