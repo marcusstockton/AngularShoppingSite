fabc2b11fcfb4c1aff64456c597d03667f37c3ac
MarcusStockton <marcus_stockton@hotmail.co.uk>
2019-07-05 19:49:45 +0100

    More Unit tests added

--- WebServer.Test/ItemControllerTests.cs
+++ WebServer.Test/ItemControllerTests.cs
@@ -16,18 +16,13 @@ namespace WebServer.Test
     [TestClass]
     public class ItemControllerTests
     {
-        private List<Item> itemList = new List<Item>();
-        private List<ItemDetails> getItems = new List<ItemDetails>();
+        private Mock<IItemsService> repo;
+        private Mock<IImageService> mockImageService;
 
         [TestInitialize]
         public void Setup()
         {
              // Runs before each test. (Optional)
-            var demoUser = new ApplicationUser{UserName="TestUser", Id = new Guid("c4af7c05-0714-49d8-a674-3e3d316f1cc9")};
-
-            itemList.Add(new Item{Id = new Guid("b754c240-817f-4e1e-97b8-6ee09700f399"), Title = "Test Title", Description = "Test Description", Price = 12.34, CreatedDate = new DateTime(2019, 01, 23), CreatedBy=demoUser});
-            itemList.Add(new Item{Id = new Guid("57fb77b1-6d69-4b8e-a424-03e3f304118d"), Title = "Another Test Title", Description = "Another Test Description", Price = 98.33, CreatedDate = new DateTime(2019, 01, 25), CreatedBy=demoUser});
-        
         }
         
         [TestCleanup]
@@ -78,6 +73,25 @@ namespace WebServer.Test
             Assert.AreEqual((int)HttpStatusCode.OK, okResult.StatusCode);
         }
 
+        [TestMethod]
+        public async Task Test_Get_Item_By_Id_With_Invalid_Id_Returns_404()
+        {
+            // Arrange
+            var mockRepo = new Mock<IItemsService>();
+            var mockImageService = new Mock<IImageService>();
+            mockRepo.Setup(x => x.GetItemById(new Guid("c8bdd28c-caf0-459b-b46c-5d3c45a38ba6"))).ReturnsAsync(new ItemDetails { Title = "Demo", Description = "Test Description", Price = 123.43});
+            var controller = new ItemsController(mockRepo.Object, mockImageService.Object);
+
+           
+            // Act
+            ActionResult<ItemDetails> result = await controller.Get("c8bdd28c-caf0-459b-b46c-5d3c45a38ba5");
+
+            // Assert
+            Assert.IsNotNull(result);
+            NotFoundResult errorResult = result.Result as NotFoundResult;
+
+            Assert.AreEqual((int)HttpStatusCode.NotFound, errorResult.StatusCode);
+        }
 
         private async Task<IEnumerable<ItemDetails>> GetItems()
         {
