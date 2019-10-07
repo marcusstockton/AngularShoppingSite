cd58d4e4153cbab8e141145e6222597240fde4ee
Marcus Stockton <marcus_stockton@hotmail.co.uk>
2019-10-07 10:33:47 +0100

    Fixed pagination issue

--- Client/src/app/items/components/items-table/items-table.component.ts
+++ Client/src/app/items/components/items-table/items-table.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, OnInit } from '@angular/core';
 import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 import { ItemsService } from '../../items.service';
 import { IItem } from '../../models/Item';
@@ -9,29 +9,23 @@ import { Router } from '@angular/router';
   templateUrl: './items-table.component.html',
   styleUrls: ['./items-table.component.css']
 })
-export class ItemsTableComponent implements AfterViewInit {
+export class ItemsTableComponent {
+
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   @ViewChild(MatSort, { static: true }) sort: MatSort;
   dataSource: MatTableDataSource<IItem>;
   ItemList: IItem[]  = [];
 
-  constructor(private service: ItemsService, private router: Router) {
-    this.service.getItems().subscribe((result: IItem[]) => {
-      this.dataSource = new MatTableDataSource(result);
-    }, (error) => {
-      console.log(error.statusText);
-    });
-  }
-
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   displayedColumns = ['Name', 'Title', 'Description', 'Price', 'CreatedDate'];
 
-  ngAfterViewInit() {
-    setTimeout(() => {
-      if (this.dataSource) {
-        this.dataSource.paginator = this.paginator;
-        this.dataSource.sort = this.sort;
-      }
+  constructor(private service: ItemsService, private router: Router) {
+    this.service.getItems().subscribe((result: IItem[]) => {
+      this.dataSource = new MatTableDataSource(result);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    }, (error) => {
+      console.log(error.statusText);
     });
   }
 
